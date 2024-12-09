e8b8384c2252d3758454e84f617e906f542f9778
ALLE <randompal320@gmail.com>
2024-12-09 01:39:06 -0500

    Got choropleth map working, started an animated scatter plot but it needs work, might scrap it. For now that's what's done. But i'm going to not focus on the animated scatter plot and FINALLY prioritize labeling,colors, and fitting it to one screen. Please god have mercy :')

--- AlejandroMichaelProject.py
+++ AlejandroMichaelProject.py
@@ -5,15 +5,75 @@ from dash import html
 from dash.dependencies import Input, Output
 import plotly.express as px
 import numpy as np
+from pandas.core.computation import scope
 
 df = pd.read_csv('USA_cars_datasets.csv')
+
+# Define the state abbreviation mapping
+state_abbreviations = {
+    'alabama': 'AL',
+    'alaska': 'AK',
+    'arizona': 'AZ',
+    'arkansas': 'AR',
+    'california': 'CA',
+    'colorado': 'CO',
+    'connecticut': 'CT',
+    'delaware': 'DE',
+    'florida': 'FL',
+    'georgia': 'GA',
+    'hawaii': 'HI',
+    'idaho': 'ID',
+    'illinois': 'IL',
+    'indiana': 'IN',
+    'iowa': 'IA',
+    'kansas': 'KS',
+    'kentucky': 'KY',
+    'louisiana': 'LA',
+    'maine': 'ME',
+    'maryland': 'MD',
+    'massachusetts': 'MA',
+    'michigan': 'MI',
+    'minnesota': 'MN',
+    'mississippi': 'MS',
+    'missouri': 'MO',
+    'montana': 'MT',
+    'nebraska': 'NE',
+    'nevada': 'NV',
+    'new hampshire': 'NH',
+    'new jersey': 'NJ',
+    'new mexico': 'NM',
+    'new york': 'NY',
+    'north carolina': 'NC',
+    'north dakota': 'ND',
+    'ohio': 'OH',
+    'oklahoma': 'OK',
+    'oregon': 'OR',
+    'pennsylvania': 'PA',
+    'rhode island': 'RI',
+    'south carolina': 'SC',
+    'south dakota': 'SD',
+    'tennessee': 'TN',
+    'texas': 'TX',
+    'utah': 'UT',
+    'vermont': 'VT',
+    'virginia': 'VA',
+    'washington': 'WA',
+    'west virginia': 'WV',
+    'wisconsin': 'WI',
+    'wyoming': 'WY'
+}
+
 
 brands = df['brand'].unique() #will be used later for graphs, get unique values of all cars ignoring duplicates.
 modifiedDf = df.drop(columns = ['Unnamed: 0']) #getting rid of 1st column since there's no header and just numbers
 dfMilesYears = modifiedDf.groupby(['mileage','price'])['year']
 
+modifiedDf['state'] = modifiedDf['state'].map(state_abbreviations)
+
 brand_of_car = modifiedDf.groupby('brand')['model'].count().reset_index().sort_values('model',ascending= False).head(10)
 brand_of_car = brand_of_car.rename(columns = {'model':'count'})
+
+car_counts_by_state = modifiedDf.groupby('state').size().reset_index(name='car_count')
 
 app = dash.Dash(__name__)
 app.layout = html.Div([
@@ -32,6 +92,38 @@ app.layout = html.Div([
     dcc.Graph(id='bar-chart',figure=px.bar(brand_of_car, x='brand',y='count', color='count', title ='Top 10 brands by %')),
     dcc.Graph(id='scatter-plot',figure=px.scatter(df, x='year', y='price', title ='Price vs Year')),
 
+    # Animated Scatter Plot
+    dcc.Graph(
+        id='animated-scatter-plot',
+        figure=px.scatter(
+            modifiedDf,
+            x='year',  # X-axis
+            y='price',  # Y-axis
+            animation_frame='year',  # Animate by year
+            animation_group='brand',  # Group by brand for smoother animations
+            size='price',  # Adjust bubble size based on price
+            color='brand',  # Color bubbles by brand
+            title='Animated Scatter Plot of Price vs Year',
+            labels={'year': 'Year', 'price': 'Price'},
+            #animation_options=dict(frame=dict(duration=500, redraw=True))  # Slow down the animation
+        )
+    ),
+
+    dcc.Graph(
+        id='choropleth-map',
+        figure=px.choropleth(
+            car_counts_by_state,
+            locations='state',  # State abbreviation or name
+            locationmode='USA-states',
+            scope= 'usa',
+            color='car_count',  # Number of cars in each state
+            hover_name='state',  # State name
+            hover_data=['car_count'],
+            color_continuous_scale='Viridis',  # Color scale for car counts
+            title="Number of Cars per State",
+            labels={'car_count':'Number of Cars'}
+        )
+    ),
     #*dcc.Dropdown(
     #  id='brand-dropdown',
     #  options=[
